d30d3a12898c65912f96f018e8db1e0d0dacfce2
Distortions81 <carlotto81@gmail.com>
2022-11-20 13:44:58 -0700

    comments

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -11,38 +10,44 @@ import (
 
 func main() {
 	var err error
+
+	/* Player starting pos/rot */
 	playerPhysics.Position.x = mapScale * 2
 	playerPhysics.Position.y = mapScale * 2
 	playerPhysics.Rotation = onePi
-	angleCalc()
+	angleCalc() /* Update movepos */
 
+	/* Window init */
 	ebiten.SetWindowSize(screenWidth*screenMag, screenHeight*screenMag)
 	ebiten.SetWindowTitle("GoRaycaster")
 
+	/* Load default test map */
 	mapImg, _, err = ebitenutil.NewImageFromFile("map1.png")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	/* Save size info */
 	xs, ys := mapImg.Size()
 	mapSize.x = xs
 	mapSize.y = ys
 
+	/* Calculate reasonable max depth */
 	maxDof = (xs * mapScale) * (ys * mapScale)
 
+	/* Precalc fov values */
 	renderFovRad = degToRad(renderFov)
 	halfFovRad = fixRad(renderFovRad / 2.0)
 	radPerRay = fixRad(renderFovRad / screenWidth)
 
+	/* Minimap setup */
 	mapRender = ebiten.NewImage(xs*mapScale, ys*mapScale)
-
-	playerPhysics.MovePos.x = math.Cos(playerPhysics.Rotation)
-	playerPhysics.MovePos.y = -math.Sin(playerPhysics.Rotation)
 	miniMapOffsetX = float64(screenWidth) - (float64(xs * mapScale))
 
 	fmt.Printf("Map size: %v,%v\n", mapSize.x, mapSize.y)
 
+	/* Start ebiten */
 	g := &Game{}
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
